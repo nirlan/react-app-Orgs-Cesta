e2138922b334d698d13991e662a1348d25bfa745
Nirlan Souza <nirlan858@gmail.com>
2021-09-22 15:27:31 -0300

    Inserção da imagem e do texto da imagem

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 
+// import do meu componente sem as chaves porque eu estou
+// importando o 'default'
 import Cesta from './src/telas/Cesta'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
+
+    // importante para IOS para que o app respeite a statusbar e uma barra de
+    // ações que tem no footer do IOS. SafeAreaView substitui a View.
+    <SafeAreaView>
+
+      {/* deletar a StatusBar do expo e usar a StatusBar do react-native */}
+      <StatusBar />
+
       {/* meu componente */}
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',    
-  },
-});
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,13 +1,53 @@
 ﻿import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+
+// importando a imagem para encontrar a pasta dela tenho que dar
+// 'cd ..' duas vezes, por isso o '../../'
+import topo from '../../assets/topo.png';
+
+// para pegar a dimensão 'width' atuais da tela
+const width = Dimensions.get('screen').width;
 
 // Para que não precise usar as chaves para importar o meu componente
 // eu uso a palavra chave 'default'. Se eu não colocar o default, eu
 // posso criar várias funções para exportar e importar no meu App.js
 // somente aquelas que eu quero usando o import com chaves '{...}'.
 export default function Cesta() {
-    return <Text>Cesta</Text>
+
+    // '<>...</> cria um fragmento sem a necessidade de criar uma View,
+    // uma outra camada para retornar a função (porque a função só pode
+    // retornar uma coisa só!) dessa forma esses elementos retornados
+    // serão irmãos da StatusBar, se eu criasse uma View nova para conter
+    // eles, essa View que eu criei que seria a irmã da StatusBar (no App.js
+    // que importa a Cesta.js).
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo}>Detalhe da cesta</Text>
+    </>
 }
+
+// StyleSheet é utilizado para criar os estilos em React Native;
+// é forma otimizada de criar os estilos do componenente.
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+
+        // Para uma altura responsiva da imagem:
+        // (altura da imagem) / (largura da imagem) * largura atual da tela;
+        // isso mantem a proporção entre altura x largura da imagem.
+        height: 578 / 768 * width, 
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+});
 
 // Nos primórdios do React Native, componentes com base em classes eram
 // a única forma de declarar componentes. Dessa forma, é criada uma classe
